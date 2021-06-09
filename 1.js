72b69c565ca62dfa391506c7f3682cb20c3ddf1e
w0930 <w15290345066@163.com>
2021-06-09 16:16:22 +0800

    测试

---
+++ 1.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-09 15:09:43
+ * @LastEditTime: 2021-06-09 15:09:51
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \-1\1.js
+ */
+console.log("你好");
